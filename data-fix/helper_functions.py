9408719cc8a72f62c2ddb9aafc68766e80baab68
JacksonBurns <JacksonBurns@users.noreply.github.com>
2021-01-05 22:55:51 -0500

    best result

--- data-fix/helper_functions.py
+++ data-fix/helper_functions.py
@@ -11,8 +11,8 @@ import glob
 import pickle
 import numpy as np
 import seaborn as sns
-import matplotlib.pylab as plt
-
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 
 class IterateSubdirectories(object):
@@ -63,7 +63,7 @@ def plot_parity(x, y,labels=None, **kwargs):
     if kwargs is not None:
         plot_params.update(kwargs)
     plt.rcParams['svg.fonttype'] = 'none'
-    plt.scatter(x=x, y=y, alpha=plot_params['alpha'], s=plot_params['s'], c=plot_params['c'])
+    plt.scatter(x=x, y=y, alpha=plot_params['alpha'], s=plot_params['s'], c=plot_params['c'], label='Test Data')
     # max_entry = max(max(x), max(y)) + plot_params.get('offset', 5)
     # min_entry = min(min(x), min(y))  - plot_params.get('offset', 5)
     max_entry = 1
@@ -72,7 +72,7 @@ def plot_parity(x, y,labels=None, **kwargs):
     axes.set_xlim([min_entry, max_entry])
     axes.set_ylim([min_entry, max_entry])
     plt.plot([min_entry, max_entry], [min_entry, max_entry],
-             color=plot_params.get('linecolor', 'black'))
+             color=plot_params.get('linecolor', 'black'),label='Perfect Model')
     plt.title(plot_params.get('title', ''), fontsize=plot_params.get('title_fontsize', 24))
     plt.xlabel(plot_params.get('xlabel', ''),
                    fontsize=plot_params.get('xlabel_fontsize', 20))
@@ -81,21 +81,45 @@ def plot_parity(x, y,labels=None, **kwargs):
     plt.xticks(fontsize=plot_params.get('xticksize',24))
     plt.yticks(fontsize=plot_params.get('yticksize',24))
 
-    plt.plot([0, 0],[-100, 100],color='black',linestyle='--')
-    plt.plot([-100, 100],[0, 0],color='black',linestyle='--')
+    plt.plot([0, 0],[-100, 100],color='black',linestyle='--',alpha=0.5)
+    plt.plot([-100, 100],[0, 0],color='black',linestyle='--',alpha=0.5)
 
     if(labels is not None):
         i = 0;
         for ix,iy in zip(x,y):
+            if(countGrossErrors(ix,iy)>0):
+                color='red'
+            else:
+                color='black'
             plt.annotate(str(labels[i]), # this is the text
                         (ix,iy), # this is the point to label
                         textcoords="offset points", # how to position the text
-                        xytext=(0,10), # distance from text to points (x,y)
-                        ha='center') # horizontal alignment can be left, right or center
+                        xytext=(0,-15), # distance from text to points (x,y)
+                        ha='center',  # horizontal alignment can be left, right or center
+                        fontsize=15,
+                        c=color)
             
             i = i + 1;
+    a = np.array([i[0] for i in x])
+    b = np.array([i[0] for i in y])
+    
+    lm = LinearRegression(fit_intercept=True)
+    lm.fit(a.reshape(-1, 1),b.reshape(-1, 1))
+    message = '''
+    Regression Results:
+    Slope = {:.2f}
+    Intercept = {:.2f}
+    R^2 = {:.2f}
+    '''.format(
+        lm.coef_[0][0],
+        lm.intercept_[0],
+        lm.score(a.reshape(-1, 1),b.reshape(-1, 1))
+    )
+    plt.annotate(message,(0.5,-0.5),ha='center',fontsize=15)
+    temp = np.array([-1,1])
+    plt.plot(temp, lm.coef_[0][0]*temp + lm.intercept_,linestyle='--',label='RBF-KPCA',c='blue')
 
-
+    plt.legend(fontsize=15)
     if plot_params.get('show_plot', True):
         plt.show()
     return plt
--- data-fix/main.py
+++ data-fix/main.py
@@ -5,12 +5,14 @@ from copy import deepcopy as dc
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from pandas.plotting import parallel_coordinates
+from seaborn import heatmap
 # data manipulation
 import numpy as np
 import pandas as pd
 # sklearn regression tools
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error as MAE
+from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, RidgeCV, LassoCV
 from sklearn.decomposition import PCA, KernelPCA
@@ -19,8 +21,10 @@ from sklearn.cluster import KMeans
 from helper_functions import plot_parity, countGrossErrors
 # wpca from https://github.com/jakevdp/wpca/
 from wpca import WPCA
+# model explaining
+import shap
 
-def loadData(zeroReplace=-1, fromXL=True, doSave=False):
+def loadData(zeroReplace=-1, fromXL=True, doSave=False, removeLessThan=None):
     """
     Retrieves data from Excel file and optionally writes it out to serialized format
     """
@@ -31,10 +35,21 @@ def loadData(zeroReplace=-1, fromXL=True, doSave=False):
         descrptr_columns = configs.get('descrptr_columns')
         err_column = configs.get('error_column')
         name_col = configs.get('ligand_names')
+        absolute_yield = configs.get('absolute_yield')
 
         df = pd.read_excel(xl_file)
+        absYields = df[absolute_yield].to_numpy()
+        
+        # remove any data which does not have our yield cutoff
+        if removeLessThan is not None:
+            remove_idxs = [i for i in range(0,len(absYields)) if absYields[i]<removeLessThan]
+            print("Removing the following ligands ({} total):".format(len(remove_idxs)), ', '.join(df[name_col].to_numpy()[remove_idxs]))
+            df.drop(remove_idxs,inplace=True)
+            print("{} ligands remaining.".format(len(df.index)))
+
         ligNames = df[name_col].to_numpy()
         y = df[target_column].to_numpy()
+        # replace any zeros to avoid inversion errors
         y[y==0] = zeroReplace
         descriptor_names = descrptr_columns
         X = df[descrptr_columns].to_numpy()
@@ -167,7 +182,7 @@ def doRidgeCV(X,y,feature_weights,sample_weights,output=False,trainSize=0.8):
     ridge = RidgeCV()
     ridge.fit(X_std_train, y_std_train.ravel(), sample_weight=s_weights_train.ravel())
     y_predict = [(_ * y_sigma) + y_scaler.mean_ for _ in ridge.predict(X_std_test)]
-    if(output and countGrossErrors(y_test,y_predict)/len(y_predict)==0 and ridge.score(X_std_train, y_std_train)>0.25 and MAE(y_true=y_test, y_pred=y_predict)<0.25):
+    if(output and countGrossErrors(y_test,y_predict)/len(y_predict)==0): # and ridge.score(X_std_train, y_std_train)>0.25 and MAE(y_true=y_test, y_pred=y_predict)<0.25):
         print('Mean Absolute Error: ', MAE(y_true=y_test,y_pred=y_predict))
         print('R2 of training data: ', ridge.score(X_std_train, y_std_train))
         print('% Gross Errors: ', countGrossErrors(y_test,y_predict)/len(y_predict))
@@ -197,7 +212,7 @@ def doLASSO(X,y,feature_weights,sample_weights,output=False,trainSize=0.8,cv=10)
     lasso = LassoCV(cv=cv,max_iter=100000)
     lasso.fit(X_std_train, y_std_train.ravel())
     y_predict = [(_ * y_sigma) + y_scaler.mean_ for _ in lasso.predict(X_std_test)]
-    if(output and countGrossErrors(y_test,y_predict)/len(y_predict)==0 and lasso.score(X_std_train, y_std_train)>0.25 and MAE(y_true=y_test, y_pred=y_predict)<0.25):
+    if(output):# and countGrossErrors(y_test,y_predict)/len(y_predict)==0 and lasso.score(X_std_train, y_std_train)>0.25 and MAE(y_true=y_test, y_pred=y_predict)<0.25):
         print('Mean Absolute Error: ', MAE(y_true=y_test,y_pred=y_predict))
         print('R2 of training data: ', lasso.score(X_std_train, y_std_train))
         print('% Gross Errors: ', countGrossErrors(y_test,y_predict)/len(y_predict))
@@ -230,12 +245,15 @@ def doKPCASeparation(X,y,vectrs=[0,1,2]):
     plt.show()
     print('Max PC1', y[np.argmax(X_kpca[:, 0])])
 
-def doKPCA(X,y,ligand_names,output=False,trainSize=0.8):
+def doKPCA(X,y,ligand_names,output=False,trainSize=0.8,heatMap=False,gamma=None,randSeed=None):
     # instantiate scalers
     x_scaler = StandardScaler()
     y_scaler = StandardScaler()
     # split response, features, and weights into train and test set
-    randState = random.randint(1,1e9)
+    if randSeed is not None:
+        randState = randSeed
+    else:
+        randState = random.randint(1,1e9)
     X_train, X_test, y_train, y_test, f_weights_train, f_weights_test, s_weights_train, s_weights_test, _, ln = train_test_split(X, y, feature_weights, sample_weights, ligand_names, train_size=trainSize, random_state=randState)
     # scale features
     X_std_train = x_scaler.fit_transform(X_train)
@@ -249,24 +267,32 @@ def doKPCA(X,y,ligand_names,output=False,trainSize=0.8):
     Radial Basis Function Kernel Principal Component Regression
     """
     # instantiate weighted PCA, train
-    kpca = KernelPCA(kernel="rbf",n_components=4)
+    kpca = KernelPCA(kernel="rbf",n_components=4,gamma=gamma)
     X_std_train = kpca.fit_transform(X_std_train)
     X_std_test = kpca.transform(X_std_test)
+    # explained_variance = np.var(X_std_train, axis=0)
+    # explained_variance_ratio = explained_variance / np.sum(explained_variance)
+    # print(np.cumsum(explained_variance_ratio))
+    # print(s_weights_train.shape)
+
+    print("Training Set: ", _)
+    print("Testing Set: ",ln)
 
     # do regression
     lm = LinearRegression()
-    lm.fit(X_std_train, y_std_train)
+    lm.fit(X_std_train, y_std_train) # , sample_weight=s_weights_train.ravel())
     y_predict = [(_ * y_sigma) + y_scaler.mean_ for _ in lm.predict(X_std_test)]
+
     y_test =[(_ * y_sigma) + y_scaler.mean_ for _ in y_std_test]
-    
-    if(output and countGrossErrors(y_test,y_predict)/len(y_predict)==0 and lm.score(X_std_train, y_std_train)>0.35 and MAE(y_true=y_test, y_pred=y_predict)<0.20):
+
+    if(output and r2_score(y_test,y_predict)>0.7): # countGrossErrors(y_test,y_predict)/len(y_predict)==0 and lm.score(X_std_train, y_std_train)>0.35 and MAE(y_true=y_test, y_pred=y_predict)<0.20):
         print('Mean Absolute Error: ', MAE(y_true=y_test,y_pred=y_predict))
         print('R2 of training data: ', lm.score(X_std_train, y_std_train))
         print('% Gross Errors: ', countGrossErrors(y_test,y_predict)/len(y_predict))
         print('Random seed for tts: ', randState)
         plot_parity(x=y_test, y=y_predict, labels=ln, xlabel='True Selectivity',ylabel='Predicted Selectivity',s=30)
 
-    return lm.score(X_std_train, y_std_train),MAE(y_true=y_test, y_pred=y_predict),countGrossErrors(y_test,y_predict)/len(y_predict)
+    return lm.score(X_std_train, y_std_train),MAE(y_true=y_test, y_pred=y_predict),countGrossErrors(y_test,y_predict)/len(y_predict), r2_score(y_test,y_predict)
 
 def doKMeansClusteringWithKPCA(X,y,sample_weights,vectrs=[0,1,2]):
     # instantiate scalers
@@ -281,7 +307,7 @@ def doKMeansClusteringWithKPCA(X,y,sample_weights,vectrs=[0,1,2]):
     """
     Cluster on the first 3 components 
     """
-    ''' this plot shows that 4 clusters is optimal
+    # this plot shows that 4 clusters is optimal
     inertiaList = []
     for i in range(1,10):
         kmeans = KMeans(n_clusters=i)
@@ -292,7 +318,7 @@ def doKMeansClusteringWithKPCA(X,y,sample_weights,vectrs=[0,1,2]):
     plt.xlabel("Number of Clusters")
     plt.title("K-Means Clustering Elbow Plot")
     plt.show()
-    '''
+    
     kmeans = KMeans(n_clusters=4)
     kmeans.fit(dc(X_pca), sample_weight=dc(sample_weights).ravel())
 
@@ -322,6 +348,59 @@ def doKMeansClusteringWithKPCA(X,y,sample_weights,vectrs=[0,1,2]):
 
     plt.show()
 
+def doKMCWithKPCAandSHAP(X,y,sample_weights,vectrs=[0,1,2],trainSize=0.8,randSeed=None):
+    # instantiate scalers
+    x_scaler = StandardScaler()
+    # split response, features, and weights into train and test set
+    if randSeed is not None:
+        randState = randSeed
+    else:
+        randState = random.randint(1,1e9)
+    X_train, X_test, y_train, y_test, s_weights_train, s_weights_test = train_test_split(X, y, sample_weights, train_size=trainSize, random_state=randState)
+    
+    # scale features
+    X_std_train = x_scaler.fit_transform(X_train)
+    X_std_test = x_scaler.transform(X_test)
+    """
+    Radial Basis Function Kernel Principal Component Regression
+    """
+    kpca = KernelPCA(kernel="rbf",n_components=3)
+    X_pca = kpca.fit_transform(X_std_train)
+    X_pca_test = kpca.transform(X_std_test)
+
+    kmeans = KMeans(n_clusters=4)
+    kmeans.fit(dc(X_pca), sample_weight=dc(s_weights_train).ravel())
+
+    # plot actual values and clustering side by side
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 2, 1, projection='3d')
+    c = [((i+1)/2,0,(i+1)/2) for i in y_train] # conditional coloring
+    ax.scatter(X_pca[:, vectrs[0]], X_pca[:, vectrs[1]], X_pca[:, vectrs[2]], \
+        c=c, alpha=0.75, s=50)
+    ax.set_xlabel(f'PC {vectrs[0] + 1}', fontsize=20)
+    ax.set_ylabel(f'PC {vectrs[1] + 1}', fontsize=20)
+    ax.set_zlabel(f'PC {vectrs[2] + 1}', fontsize=20)
+    # clustering
+    ax = fig.add_subplot(1, 2, 2, projection='3d')
+    colors = ['red','green','blue','orange']
+    c = [colors[i] for i in kmeans.labels_] # conditional coloring
+    ax.scatter(X_pca[:, vectrs[0]], X_pca[:, vectrs[1]], X_pca[:, vectrs[2]], \
+        c=c, alpha=0.75, s=50)
+    ax.set_xlabel(f'PC {vectrs[0] + 1}', fontsize=20)
+    ax.set_ylabel(f'PC {vectrs[1] + 1}', fontsize=20)
+    ax.set_zlabel(f'PC {vectrs[2] + 1}', fontsize=20)
+    for label in range(0,4):
+        print('Cluster {} ({}) average and stdev:'.format(label,colors[label]))
+        print(np.average(y_train[kmeans.labels_ == label]))
+        print(np.std(y_train[kmeans.labels_ == label]))
+        print('Max in this Cluster: ',np.max(y_train[kmeans.labels_ == label]))
+
+    plt.show()
+
+    explainer = shap.KernelExplainer(kmeans.predict,X_pca)
+    shap_vals = explainer.shap_values(X_pca_test)
+    shap.summary_plot(shap_vals,X_pca_test)
+
 def doKMeansClustering(X,y,sample_weights,output=False,varianceNeeded=0.95):
     """
     K-Means Clustering
@@ -337,165 +416,28 @@ def doKMeansClustering(X,y,sample_weights,output=False,varianceNeeded=0.95):
         print('Max in this Cluster: ',np.max(y[kmeans.labels_ == label]))
 
 if __name__ == '__main__':
-    X, y, feature_weights, sample_weights, ligNames =  loadData(zeroReplace=0.01)
+    X, y, feature_weights, sample_weights, ligNames =  loadData(zeroReplace=0.01,removeLessThan=2)
     # familySeparation(dc(X),dc(y),dc(ligNames))
-    # R2s=[]; maes=[]; GEs=[];
-    # for i in range(0,10000):
-        # R2, mae, GE = doWPCA(dc(X),dc(y),dc(feature_weights),dc(sample_weights),output=False)
+    # R2s=[]; MAEs=[]; GEs=[]; testR2s=[];
+    # for i in range(0,1000):
+        # R2, mae, GE = doWPCA(dc(X),dc(y),dc(feature_weights),dc(sample_weights),output=True)
         # R2, mae, GE = doRidgeCV(dc(X),dc(y),dc(feature_weights),dc(sample_weights),output=True)
         # R2, mae, GE = doLASSO(dc(X),dc(y),dc(feature_weights),dc(sample_weights),output=True)
-        # R2, mae, GE = doKPCA(dc(X),dc(y),dc(ligNames),output=True)
-        # R2s.append(R2); maes.append(mae); GEs.append(GE);
+    R2, mae, GE, alsoR2 = doKPCA(dc(X),dc(y),dc(ligNames),output=True,heatMap=False, randSeed=837262349)
+        # R2s.append(R2); MAEs.append(mae); GEs.append(GE); testR2s.append(alsoR2);
     # doKPCASeparation(dc(X),dc(y))
     # doKMeansClusteringWithKPCA(dc(X),dc(y),dc(sample_weights))
+    # doKMCWithKPCAandSHAP(dc(X),dc(y),dc(sample_weights),randSeed=454691077)
     # doKMeansClustering(dc(X),dc(y),dc(sample_weights))
-    
+
     # plt.figure()
-    # plt.hist(GEs,rwidth=0.9,bins=[i/20 for i in range(0,20)],align='left')
-    # plt.title('RBF-KPCA Distribution of %GE (1000 runs, 80:20 training:test)')
+    # plt.hist(testR2s,rwidth=0.9,bins=[i/20 for i in range(0,20)],align='left')
+    # plt.title('RBF-KPCA Distribution of Testing R^2 (1000 runs, 80:20 training:test)')
     # plt.ylabel('Frequency')
-    # plt.xlabel('%GE')
+    # plt.xlabel('R^2')
     # plt.xticks([i/20 for i in range(0,20)],[str(i) for i in range(0,96,5)])
     # plt.show()
     # print("""Average %GE: {:.2f} Standard Deviation: {:.2f}""".format(np.average(GEs),np.std(GEs)))
-
-"""
-3-Means Clustering on Scaled Data:
-Cluster 0 average and stdev:
-0.30469411214281666
-0.40053104923738647
-Max in this Cluster:  1.0
-Cluster 1 average and stdev:
--0.23992603326009596
-0.4550882101556402
-Max in this Cluster:  0.9242463079717002
-Cluster 2 average and stdev:
--0.38072314747564323
-0.3468476014150291
-Max in this Cluster:  0.1530147587934812
-
-K-Means Clustering on RBF 3-Component PCA
-Cluster 0 (red) average and stdev:
-0.36942100042981535
-0.35070508549836693
-Cluster 1 (green) average and stdev:
--0.23043421338312967
-0.5191292407314995
-Cluster 2 (blue) average and stdev:
--0.2162061107937193
-0.4289383248262524
-Cluster 3 (orange) average and stdev:
--0.3453850894798298
-0.3604713124172736
-
-
-RBF KPCA w/ 3 Components:
-Mean Absolute Error:  0.25915683583389704
-R2 of training data:  0.3532508522886406
-% Gross Errors:  0.0
-Random seed for tts:  270252416
-Mean Absolute Error:  0.26303986364507775
-R2 of training data:  0.3689261497471472
-% Gross Errors:  0.0
-Random seed for tts:  220232570
-Mean Absolute Error:  0.2843560462696692
-R2 of training data:  0.3455074211212217
-% Gross Errors:  0.0
-Random seed for tts:  945107195
-Mean Absolute Error:  0.3194705614025062
-R2 of training data:  0.3391373329330386
-% Gross Errors:  0.0
-Random seed for tts:  536367301
-Average %GE: 0.26 Standard Deviation: 0.09
-
-RBF KPCA w/ 4 Components:
-Mean Absolute Error:  0.2819107574618129
-R2 of training data:  0.3977701391835511
-% Gross Errors:  0.0
-Random seed for tts:  449119043
-Mean Absolute Error:  0.30894735554882496
-R2 of training data:  0.35324491555535087
-% Gross Errors:  0.0
-Random seed for tts:  82702285
-Mean Absolute Error:  0.2205499763350409
-R2 of training data:  0.37968097604140394
-% Gross Errors:  0.0
-Random seed for tts:  776208407
-Average %GE: 0.26 Standard Deviation: 0.09
-
-Mean Absolute Error:  0.1814146128716552
-R2 of training data:  0.3819042574118835
-% Gross Errors:  0.0
-Random seed for tts:  404994264
-Mean Absolute Error:  0.22850724914755016
-R2 of training data:  0.3805744044531153
-% Gross Errors:  0.0
-Random seed for tts:  675509436
-Mean Absolute Error:  0.1816055690023437
-R2 of training data:  0.3651971794096397
-% Gross Errors:  0.0
-Random seed for tts:  222984316
-
-Mean Absolute Error:  0.19716148549345128
-R2 of training data:  0.3587982346313736
-% Gross Errors:  0.0
-Random seed for tts:  475649123
-Mean Absolute Error:  0.18682496488248101
-R2 of training data:  0.3876984353979501
-% Gross Errors:  0.0
-Random seed for tts:  150029923
-Mean Absolute Error:  0.19950748807308485
-R2 of training data:  0.37626429037093834
-% Gross Errors:  0.0
-Random seed for tts:  41520904
-
-Mean Absolute Error:  0.19854339523087197
-R2 of training data:  0.3591234736678204
-% Gross Errors:  0.0
-Random seed for tts:  394590766
-
-Mean Absolute Error:  0.19964926049819112
-R2 of training data:  0.3848822686072071
-% Gross Errors:  0.0
-Random seed for tts:  454691077
-
-WPCR:
-Best results so far:
-0.9505432719216261 explained by 3 eigen-vectors
-Mean Absolute Error:  0.2533452596352515
-R2 of training data:  0.2707783943385329
-% Gross Errors:  0.0
-Random seed for tts:  632130344
-
-0.9662726637817706 explained by 5 eigen-vectors
-Mean Absolute Error:  0.23734940231762897
-R2 of training data:  0.260765038246244
-% Gross Errors:  0.0
-Random seed for tts:  766178200
-
-0.9587033761934431 explained by 4 eigen-vectors
-Mean Absolute Error:  0.23388469610427456
-R2 of training data:  0.27047264455249775
-% Gross Errors:  0.0
-Random seed for tts:  355646495
-
-Distribution of MAE's:
-Average %GE: 0.25 Standard Deviation: 0.09
-
-RidgeCV:
-0.37+-.10
-
-LASSO:
-Average %GE: 0.25 Standard Deviation: 0.09
-
-RBF KPCA:
-Average %GE: 0.36 Standard Deviation: 0.10
-
-Cosine KPCA:
-Average %GE: 0.37 Standard Deviation: 0.11
-
-Poly d2:
-Average %GE: 0.37 Standard Deviation: 0.10
-Poly d3:
-
-"""
+    # print("""Average R2: {:.2f} Standard Deviation: {:.2f}""".format(np.average(R2s),np.std(R2s)))
+    # print("""Average MAE: {:.2f} Standard Deviation: {:.2f}""".format(np.average(MAEs),np.std(MAEs)))
+    # print("""Average Testing R2: {:.2f} Standard Deviation: {:.2f}""".format(np.average(testR2s),np.std(testR2s)))
